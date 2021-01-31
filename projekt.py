8db8d8ed205fd440063a5c3b47e138e72e9f3c55
avrkic <77241612+avrkic@users.noreply.github.com>
2021-01-31 21:11:31 +0100

    Update projekt.py

--- projekt.py
+++ projekt.py
@@ -1,1 +1,8 @@
 import random
+igrase = True
+
+class PrikazIgre (object):
+    def pikaziPocetakIgre(self):
+        print("*" * 50)
+        print("*" * 20 + "IGRA 21" + "*" * 20)
+        print("*" *50)
